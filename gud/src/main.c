dd68a154bec0aca5f99d4df9978ae558745d3112
loconali cimat <emilio.romerodlsantos@gmail.com>
2018-03-06 18:16:37 -0600

    Hacer correcion de lectura de matriz de conectividad

--- gud/src/main.c
+++ gud/src/main.c
@@ -14,9 +14,17 @@ int maxiter, dim, npe, nelem;
 printf("...........\n Se leera el archivo: %s \n..............\n",ain);
 datosMalla(ain,problem,solucionador,&tol,&maxiter,&dim,&npe,&nelem);
 
-matrizConectividad(ain,npe,nelem);
+int **mc=matrizConectividad(ain,npe,nelem);
+for(int i=0;i<5;i++)
+  printf("%d %d %d\n",mc[i][0],mc[i][1],mc[i][2]);
 
 printf("...........\n Se se escribira el archivo: %s \n..............\n",aout);
+
+/*Liberacion de memoria*/
+for(int i=0;i<nelem;i++){
+  free(mc[i]);
+}
+free(mc);
 
 printf("Gracias por usar gud!\nThanks for using gud!\n");
 return(0);}
